94fa60e06e798736fbb453abee077b905faa3936
lamiyahell <axira1108@gmail.com>
2023-08-06 11:20:48 +0300

    1 commit

---
+++ src/main/java/sky/pro/CalculatorWebApp/CalculatorWebAppApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.CalculatorWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/CalculatorWebApp/CalculatorWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.CalculatorWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
